ab831de6789554461d25c4bb03dd464d7532867d
DarFelix <xbxb78@gmail.com>
2023-01-13 14:30:05 -0500

    carga

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Article, SearchService } from './pages/search/services/search.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  articles$ !: Observable<Article[]>;
+  constructor(private readonly searchSvc: SearchService){
+  }
+
+  onSearch(term:string):void{
+    this.articles$ = this.searchSvc.search(term);
+  }
+}
